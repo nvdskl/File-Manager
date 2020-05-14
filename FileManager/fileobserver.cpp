ed1ddc9f050f61072d227ae3d5137a87ded4e339
maria <89131140060@mail.ru>
2020-05-14 17:02:23 +0700

    File Observer

---
+++ FileManager/fileobserver.cpp
@@ -0,0 +1,52 @@
+#include "fileobserver.h"
+#include <QFile>
+#include <QFileInfo>
+#include <QTextStream>
+
+using namespace std;
+
+FileObserver::FileObserver(QObject *parent) : QObject(parent)
+{
+}
+
+FileObserver::FileObserver(const QString &filename)
+{
+    this->filename = filename;
+    QFileInfo info(filename);
+    exist = info.exists();
+    size = info.size();
+}
+
+void FileObserver::setFile(const QString &filename)
+{
+    this->filename = filename;
+}
+
+void FileObserver::aboutFile()
+{
+    QTextStream out(stdout);
+    if (exist)
+    {
+        QString info = QString("Filename: %1, Size: %2, is_exist: true")
+                .arg(filename).arg(size);
+        out << info << "\n";
+    }
+    else
+    {
+        out << "File " << filename << " doesn't exist" << "\n";
+    }
+
+    out.flush();
+}
+
+void FileObserver::updateInfo(QString filename, qint64 size, bool exist)
+{
+    if (this->filename == filename)
+    {
+        this->filename = filename;
+        this->size = size;
+        this->exist = exist;
+
+        aboutFile();
+    }
+}
